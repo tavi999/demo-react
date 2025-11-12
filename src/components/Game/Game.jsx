df249f29d6bf6d6e27888e792a93587b55263ebd
tavi999 <taviposta@gmail.com>
2025-11-12 16:08:03 +0200

    Updated Game.jsx, added currentMove constant index for the moves history, xIsNext now is simply true for even currentMove and false for odd, currentSquares now are initialized with history[currentMove], added jumpTo function that resets the currentMove to whichever it has as argument, added constant moves that is a history mapped items list displayed

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,15 +2,36 @@ import { Board } from "../Board/Board"
 import {useState} from "react";
 
 export function Game(){
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
     function handlePlay(nextSquares){
-
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
     }
+
+    function jumpTo(nextMove){
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if(move > 0){
+            description = 'Go to move #' + move;
+        }else{
+            description = 'Go to game start';
+        }
+        return (
+            <li key = {move}>
+                <button onClick = {() => jumpTo(move)}>
+                    {description}
+                </button>
+            </li>
+        );
+    });
 
     return (
         <div className="game">
@@ -18,7 +39,7 @@ export function Game(){
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             </div>
             <div className="game-info">
-
+                <ol>{moves}</ol>
             </div>
         </div>
     )
